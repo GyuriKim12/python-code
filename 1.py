846547dcdddc7c15cb1d8fc60c6c20dffec7a274
GyuriKim12 <kralicia@naver.com>
2021-03-19 02:48:43 +0900

    first commit

---
+++ 1.py
@@ -0,0 +1,41 @@
+a = [1,2,3,['a','b','c']]
+print(a[0])
+print(a[-1])
+print(a[-1][2])
+
+a = 'hobby'
+print(a.count('b')) #b가 몇번 나오는지
+
+a = 'python is the best choice.'
+print(a.find('b')) #b가 문자열 중 어디에 있는지
+print(a.find('k')) #k가 없으면 -1나옴
+
+a = 'Life is too short'
+print(a.index('t')) # 없는 것을 넣으면 에러나옴
+
+a = '.'.join('abcd')
+print(a)
+
+
+a,b = input('숫자 2개를 입력하세요: ').split() #공백을 기준으로 할당
+print(a)
+print(b)
+
+a = 'apple, banna, karrot'.split(,) 
+b,c,d = a.split(',') #콤마를 기준으로 할당
+print(b)
+print(c)
+print(d)
+
+x,y,z = map(int(input("숫자 3개를 입력하세요: ")).split())
+small = 0
+if (x>y):
+    small=y
+    if(y>z):
+        small=z
+elif (x<y):
+    small=x
+    if(x>z):
+        small=z
+print(small)
+"가장 작은 값은 "+str(small)+" 입니다."
---
+++ 2.py
@@ -0,0 +1,61 @@
+a = [1,2,3,['a','b','c']]
+print(a[1])
+print(a[-1])
+print(a[3][-2])
+
+a = [1.3,2.5,3.9,4.1]
+a = list(map(int,a))
+print(a)
+
+a = input("숫자 2개를 입력하시오: ").split()
+print(a)
+
+print("안녕하세요\n저는 김규리 입니다.")
+print("안녕하세요\r저는 김규리 입니다.") 
+print("\"안녕하세요 저는 김규리 입니다.\"")
+print("\'안녕하세요 저는 김규리 입니다.\'")
+
+for i in range(5):
+    print(i)
+
+a = "i wanna go to house"
+print(a.count('a'))
+print(a.find('a'))
+print(a.index('a'))
+
+a = '.'.join('abcd')
+print(a)
+
+print(list(range(10)))
+
+i = 5
+while i>3:
+    print('i= ', i)
+    i= i-1
+
+str1 = 'hellow'
+for val in str1:
+    print(val, str1)
+
+x=4
+for i in range(0,x):
+    print(i)
+    x=5
+
+x=4
+for i in range(2,x):
+    print(i)
+
+x=5
+for i in range(x):
+    for i in range(x):
+        print(i)
+    x=3
+    
+a = ['apple','lemon','orange','cherry']
+for val in a:
+    print(val,len(val))
+
+
+
+
