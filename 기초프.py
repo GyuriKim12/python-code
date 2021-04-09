a2028b3813e2ad9db5f73fba109c5c1a542af5b3
GyuriKim12 <kralicia@naver.com>
2021-04-10 04:46:59 +0900

    commit

--- 기초프.py
+++ 기초프.py
@@ -1,18 +1,21 @@
-x=int(input('Input number: '))
-epsilon = 0.01
-low = 0
-high = max(1.0,x)
-x_squrt = (high + low)/2.0
-while abs(x_squrt**2 - x) >= epsilon: 
-    x_squrt = (high + low)/2.0
-    print('low =', str(low)+',', 'high =', str(high)+',', 'x_squrt =', x_squrt)
-    if x_squrt**2 < x:
-        low = x_squrt 
-    else:
-        high = x_squrt 
+def isInt(x):
+    result=True
+    for i in x:
+        if x in "0123456789":
+            result=False
+        else:
+            result=True
+            break
+    return result
 
-sum=0
-for i in range(3):
-    x=int(input('Input an integer: '))
-    sum+=x
-print(sum)
+def InputInt(s):
+    while True:
+        x=input('Enter an integer: ')
+        ans=isInt(x)
+        if ans==True:
+            break
+        else:
+            print('Wrong integer! Input the',s,'integer again!')
+        return int(x)
+
+def 
